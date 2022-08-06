e3cbfd4c4507a93a06f062c2c94f0e1a010876e3
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-08-06 20:04:03 +0100

    Added Reaction and Thought Models

---
+++ src/models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const { formatTimestamp } = require("../utils");
+
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = {
+	thoughtText: {
+		type: String,
+		required: true,
+		minLength: 1,
+		maxLength: 100,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+		get: formatTimestamp,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	reactions: [reactionSchema],
+};
+
+const schema = new Schema(thoughtSchema, {
+	toJSON: {
+		getters: true,
+	},
+	id: false,
+});
+
+schema.virtual("reactionCount").get(function () {
+	return this.reactions.length;
+});
+
+const Thought = model("thought", schema);
+
+module.exports = Thought;
