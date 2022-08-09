6327fe58526c5ce525768aa24e269d891fe721c4
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-08-09 18:37:02 +0100

    Created placeholder controller fns for thoughts

---
+++ src/controllers/api/thoughts.js
@@ -0,0 +1,15 @@
+const { Thought } = require("../../models");
+
+const getThoughts = (req, res) => {};
+const getThoughtById = (req, res) => {};
+const createThought = (req, res) => {};
+const updateThoughtById = (req, res) => {};
+const deleteThoughtById = (req, res) => {};
+
+module.exports = {
+	getThoughts,
+	getThoughtById,
+	createThought,
+	updateThoughtById,
+	deleteThoughtById,
+};
