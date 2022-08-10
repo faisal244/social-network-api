fde2ea4ec720cd7d68c04f84130d6da0df90c5c8
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-08-10 18:56:04 +0100

    Completed all controller functions for thoughts
    - Completed getThoughtById ( )
    - Completed createThought ( )
    - Completed updateThoughtById ( )
    - Completed deleteThoughtById ( )
    
    - Used try/catch statements in each controller function

--- src/controllers/api/thoughts.js
+++ src/controllers/api/thoughts.js
@@ -12,10 +12,69 @@ const getThoughts = async (req, res) => {
 	}
 };
 
-const getThoughtById = async (req, res) => {};
-const createThought = async (req, res) => {};
-const updateThoughtById = async (req, res) => {};
-const deleteThoughtById = async (req, res) => {};
+const getThoughtById = async (req, res) => {
+	try {
+		const { thoughtId } = req.params;
+		const data = await Thought.findById(thoughtId);
+		return res.json({ success: true, data });
+	} catch (error) {
+		console.log(`[ERROR]: Failed to get thought | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to get thought" });
+	}
+};
+
+const createThought = async (req, res) => {
+	try {
+		const { userName, thoughtText } = req.body;
+		if (userName && thoughtText) {
+			const data = await Thought.create({ userName, thoughtText });
+			return res.json({ success: true, data });
+		}
+		return res.status(400).json({
+			success: false,
+			error: "Please complete the required fields",
+		});
+	} catch (error) {
+		console.log(`[ERROR]: Failed to create new thought | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to create new thought" });
+	}
+};
+
+const updateThoughtById = async (req, res) => {
+	try {
+		const { thoughtId } = req.params;
+		const data = await Thought.findByIdAndUpdate(
+			thoughtId,
+			{
+				...req.body,
+			},
+			{ new: true }
+		);
+		return res.json({ success: true, data });
+	} catch (error) {
+		console.log(`[ERROR]: Failed to update thought | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to update thought" });
+	}
+};
+
+const deleteThoughtById = async (req, res) => {
+	try {
+		const { thoughtId } = req.params;
+		const deletedThought = await Thought.findByIdAndDelete(thoughtId);
+		return res.json({ success: true, data: deletedThought });
+	} catch (error) {
+		console.log(`[ERROR]: Failed to delete thought | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Please provide correct ID" });
+	}
+};
 
 module.exports = {
 	getThoughts,
