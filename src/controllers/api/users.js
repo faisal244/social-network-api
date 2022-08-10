95c7b3dde38d794e1ae215b64709d4a2b6002a94
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-08-10 17:31:58 +0100

    Completed all controller functions for users
    - Completed getUserById ( )
    - Completed createUser ( )
    - Completed updateUserById ( )
    - Completed deleteUserById ( )
    
    - Used try/catch statements in each controller function

--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -12,13 +12,71 @@ const getUsers = async (req, res) => {
 	}
 };
 
-const getUserById = async (req, res) => {};
+const getUserById = async (req, res) => {
+	try {
+		const { userId } = req.params;
+		const data = await User.findById(userId);
+		// .populate("thoughts")
+		// .populate("friends");
+		return res.json({ success: true, data });
+	} catch (error) {
+		console.log(`[ERROR]: Failed to get user | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to get user" });
+	}
+};
 
-const createUser = async (req, res) => {};
+const createUser = async (req, res) => {
+	try {
+		const { userName, email } = req.body;
+		if (userName && email) {
+			const data = await User.create({ userName, email });
+			return res.json({ success: true, data });
+		}
+		return res.status(400).json({
+			success: false,
+			error: "Please complete the username and email",
+		});
+	} catch (error) {
+		console.log(`[ERROR]: Failed to create new user | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to create new user" });
+	}
+};
 
-const updateUserById = async (req, res) => {};
+const updateUserById = async (req, res) => {
+	try {
+		const { userId } = req.params;
+		const data = await User.findByIdAndUpdate(
+			userId,
+			{
+				...req.body,
+			},
+			{ new: true }
+		);
+		return res.json({ success: true, data });
+	} catch (error) {
+		console.log(`[ERROR]: Failed to update existent user | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to update existent user" });
+	}
+};
 
-const deleteUserById = async (req, res) => {};
+const deleteUserById = async (req, res) => {
+	try {
+		const { userId } = req.params;
+		const data = await User.findByIdAndDelete(userId);
+		return res.json({ success: true, data });
+	} catch (error) {
+		console.log(`[ERROR]: Failed to delete user | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Please provide correct ID" });
+	}
+};
 
 module.exports = {
 	getUsers,
