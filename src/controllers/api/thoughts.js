0ac4c0e22f90e6ce5dbbe88cc5fa6df98e0d5024
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-08-10 13:33:13 +0100

    Completed getThoughts controller function

--- src/controllers/api/thoughts.js
+++ src/controllers/api/thoughts.js
@@ -1,6 +1,17 @@
 const { Thought } = require("../../models");
 
-const getThoughts = async (req, res) => {};
+const getThoughts = async (req, res) => {
+	try {
+		const data = await Thought.find({});
+		return res.json({ success: true, data });
+	} catch (error) {
+		console.log(`[ERROR]: Failed to get thoughts | ${error.message}`);
+		return res
+			.status(500)
+			.json({ success: false, error: "Failed to get thoughts" });
+	}
+};
+
 const getThoughtById = async (req, res) => {};
 const createThought = async (req, res) => {};
 const updateThoughtById = async (req, res) => {};
